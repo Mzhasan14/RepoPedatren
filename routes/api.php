0e3a58c770e947b411b112f8cfbc173b698ef8b1
fjer <fhm.pribadi@gmail.com>
2025-02-04 20:08:57 +0700

    menambahkan route status keluarga

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\api\SantriController;
 use App\Http\Controllers\api\BiodataController;
-
+use App\Http\Controllers\api\StatusKeluargaController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -16,3 +16,5 @@ Route::apiResource('/santri', SantriController::class);
 Route::apiResource('/keluarga',KeluargaController::class);
 
 Route::apiResource('/biodata',BiodataController::class);
+
+Route::apiResource('/status_keluarga',StatusKeluargaController::class);
