996211ecc74b24c1dfe3f093422a371c47029e34
AbdulFatah1309 <af434613@gmail.com>
2025-03-11 12:03:04 +0700

    membuat api Query parameter / filtering dan sedikit merubah Api pengurus

--- app/Http/Controllers/api/Pegawai/PengurusController.php
+++ app/Http/Controllers/api/Pegawai/PengurusController.php
@@ -2,14 +2,22 @@
 
 namespace App\Http\Controllers\Api\Pegawai;
 
+use App\Http\Controllers\api\FilterController;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\PdResource;
 use App\Models\Pegawai\Pengurus;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 
 class PengurusController extends Controller
 {
+    protected $filterController;
+
+    public function __construct(FilterController $filterController)
+    {
+        $this->filterController = $filterController;
+    }
     /**
      * Display a listing of the resource.
      */
@@ -82,21 +90,54 @@ class PengurusController extends Controller
         return new PdResource(true,'Data berhasil ditampilkan',$pengurus);
 
     }
-    public function dataPengurus()
+    public function dataPengurus(Request $request)
     {
-        $pengurus = Pengurus::Active()
+        $query = Pengurus::Active()
                             ->join('golongan','pengurus.id_golongan','=','golongan.id')
                             ->join('kategori_golongan','golongan.id_kategori_golongan','=','kategori_golongan.id')
                             ->join('pegawai','pengurus.id_pegawai','pegawai.id')
-                            ->join('biodata','pegawai.id_biodata','=','biodata.id')
-                            ->select(
-                                'biodata.id as id',
-                                'biodata.nama as Nama',
-                                'biodata.nik as NIK',
-                                'golongan.nama_golongan as Jabatan',
-                                'kategori_golongan.nama_kategori_golongan as Golongan Jabatan'
-                            )->get();
-        return new PdResource(true,'list data berhasil di tampilkan',$pengurus);
+                            ->join('biodata','pegawai.id_biodata','=','biodata.id');
+       $query = $this->filterController->applyCommonFilters($query, $request);
+    //    satuan kerja soon
+        if ($request->filled('golongan')) {
+            $query->where('golongan.nama_golongan', $request->golongan);
+        }
+        if ($request->filled('kategori_golongan')) {
+            $query->where('kategori_golongan.nama_kategori_golongan', $request->kategori_golongan);
+        }
+    //     pemberkasan soon
+    //      warga pesantren soon
+    if ($request->filled('umur')) {
+        $umurInput = $request->umur;
+    
+        // Cek apakah input umur dalam format rentang (misalnya "20-25")
+        if (strpos($umurInput, '-') !== false) {
+            [$umurMin, $umurMax] = explode('-', $umurInput);
+        } else {
+            // Jika input angka tunggal, jadikan batas atas dan bawah sama
+            $umurMin = $umurInput;
+            $umurMax = $umurInput;
+        }
+    
+        // Filter berdasarkan umur yang dihitung dari tanggal_lahir
+        $query->whereBetween(
+            DB::raw('TIMESTAMPDIFF(YEAR, biodata.tanggal_lahir, CURDATE())'),
+            [(int)$umurMin, (int)$umurMax]
+        );
+    }
+        if ($request->filled('no_telepon')){
+            $query->where('biodata.no_telepon',$request->no_telepon);
+        }
+
+        $hasil = $query                           
+        ->select(
+            'pengurus.id as id',
+            'biodata.nama as Nama',
+            'biodata.nik as NIK',
+            'golongan.nama_golongan as Jabatan',
+            'kategori_golongan.nama_kategori_golongan as Golongan Jabatan'
+        )->paginate(25);
+        return new PdResource(true,'list data berhasil di tampilkan',$hasil);
                             
     }
 }
