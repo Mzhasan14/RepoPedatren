774f1934d2571d95ea4bc47912e83dc14bc696f9
muhzain <mzhasan1404@gmail.com>
2025-10-18 10:14:33 +0700

    fix seeder jenis berkas

--- database/seeders/JenisBerkasSeeder.php
+++ database/seeders/JenisBerkasSeeder.php
@@ -16,24 +16,15 @@ class JenisBerkasSeeder extends Seeder
         //
         // (new JenisBerkasFactory())->count(5)->create();
         $jenisBerkas = [
-            ['nama_jenis_berkas' => 'Kartu Keluarga (KK)'],
-            ['nama_jenis_berkas' => 'KTP/KIA'],
-            ['nama_jenis_berkas' => 'Akte Kelahiran'],
             ['nama_jenis_berkas' => 'Pas Foto'],
+            ['nama_jenis_berkas' => 'Kartu Keluarga (KK)'],
+            ['nama_jenis_berkas' => 'Akte Kelahiran'],
             ['nama_jenis_berkas' => 'Ijazah Terakhir'],
-            ['nama_jenis_berkas' => 'Fotokopi Rapor Terakhir'],
+            ['nama_jenis_berkas' => 'Rapor Terakhir'],
+            ['nama_jenis_berkas' => 'KTP/KIA'],
             ['nama_jenis_berkas' => 'Surat Keterangan Sehat'],
-            ['nama_jenis_berkas' => 'BPJS / Kartu Asuransi Kesehatan'],
-            ['nama_jenis_berkas' => 'Surat Pernyataan Kesanggupan'],
-            ['nama_jenis_berkas' => 'Surat Izin Orang Tua'],
-            ['nama_jenis_berkas' => 'Surat Pernyataan Tidak Merokok'],
-            ['nama_jenis_berkas' => 'Surat Keterangan Pindah Sekolah'],
+            ['nama_jenis_berkas' => 'Kartu Indonesia Pintar (KIP)'],
             ['nama_jenis_berkas' => 'Surat Keterangan Lulus (SKL)'],
-            ['nama_jenis_berkas' => 'Surat Rekomendasi dari Ulama/Guru'],
-            ['nama_jenis_berkas' => 'Surat Pernyataan Bebas Narkoba'],
-            ['nama_jenis_berkas' => 'Surat Domisili (jika dari luar kota)'],
-            ['nama_jenis_berkas' => 'Surat Keterangan Anak Yatim/Piatu'],
-            ['nama_jenis_berkas' => 'Fotokopi Kartu Santri'],
         ];
 
         foreach ($jenisBerkas as $berkas) {
